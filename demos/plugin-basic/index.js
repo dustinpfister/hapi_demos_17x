dd8a586714ea4edf3dc93e5a893d4be9795d042f
dustinpfister <dustin.pfister@gmail.com>
2019-06-25 10:16:51 -0400

    clean up

--- demos/plugin-basic/index.js
+++ demos/plugin-basic/index.js
@@ -1,13 +1,9 @@
 let Hapi = require('@hapi/hapi');
 
-let myPlugin = {
+let pluginRoot = {
     name: 'myPlugin',
     version: '1.0.0',
     register: async function (server, options) {
-
-        // Create a route for example
-
-
         server.route({
             method: 'GET',
             path: '/',
@@ -16,44 +12,21 @@ let myPlugin = {
                 return 'hello world this is ' + options.mess;
             }
         });
-
-        // etc ...
-        //await someAsyncMethods();
     }
 };
 
 let init = async() => {
-
     let server = Hapi.server({
             port: 3000,
             host: 'localhost'
         });
-/*
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-
-            return 'Hello World!';
-        }
-    });
-	*/
-
     await server.register({
-        plugin: myPlugin,
+        plugin: pluginRoot,
         options: {
             mess: 'foobar'
         }
     });
-
     await server.start();
     console.log('Server running on %s', server.info.uri);
 };
-
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
-});
-
 init();

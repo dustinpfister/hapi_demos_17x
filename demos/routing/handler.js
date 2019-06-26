29cc3f1524723637609150a6b3d6033335ba1baa
dustinpfister <dustin.pfister@gmail.com>
2019-06-26 19:36:04 -0400

    handler.js

---
+++ demos/routing/handler.js
@@ -0,0 +1,36 @@
+let Hapi = require('@hapi/hapi');
+
+let init = async() => {
+    let server = Hapi.server({
+            port: process.env.PORT || process.argv[2] || 3000,
+            host: 'localhost'
+        });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        // a handler can just return a static
+        // string
+        handler: function (request, h) {
+            return '<a href=\"/rresponse\">response</a>';
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/response',
+        // Another options is the h.response
+        // response toolkit method
+        handler: function (request, h) {
+            response = h.response({
+                    foo: 'bar'
+                });
+            response.type('application/json');
+            return response;
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+init();

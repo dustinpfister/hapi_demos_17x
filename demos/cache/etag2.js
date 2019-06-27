e1b41afc6b3616497aed3560a454d4e16b5477be
dustinpfister <dustin.pfister@gmail.com>
2019-06-27 12:18:29 -0400

    etag2

--- demos/cache/etag2.js
+++ demos/cache/etag2.js
@@ -8,19 +8,34 @@ let init = async() => {
 
     // etag based on the time the server started
     let startTime = new Date(),
-    etag = startTime.getTime();
+    etag_server = String(startTime.getTime());
 
     server.route({
         method: 'GET',
         path: '/',
         handler: (request, h) => {
+
+            // using etag
             h.entity({
-                etag: etag
+                etag: etag_server
             });
-            return {
-                mess: 'date should only change each time the sever starts',
-                date: new Date()
-            };
+
+            // get client etag
+            let etag_client = request.headers['if-none-match'];
+            etag_client = etag_client === undefined ? 0 : etag_client.replace(/"/g, '');
+
+            // send response based on etag
+            if (etag_client === etag_server) {
+                console.log('request for fresh resource');
+                return {};
+            } else {
+                console.log('old cached copy sending the new stuff...');
+                return {
+                    mess: 'date should only change each time the sever starts',
+                    date: new Date()
+                };
+
+            }
         }
     });
     await server.start();
